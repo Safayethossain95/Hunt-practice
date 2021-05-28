18b450887a419f56bffff2fd8f6e9eab06c0084f
Safayethossain95 <safayet.hossain95@gmail.com>
2021-05-28 23:20:03 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+     $('.slick-slider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          arrows:true,
+          prevArrow:'.left',
+          nextArrow:'.right',
+          pauseOnHover:false,
+        });
+}
