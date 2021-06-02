f2ad55d9b67195699c8c83cf00a059801a0a2d3f
Safayethossain95 <safayet.hossain95@gmail.com>
2021-06-02 23:33:32 +0600

    service1

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,22 @@ $('.vai_slider').slick({
     nextArrow:"<i class='fa fa-chevron-right right'></i>",
 
   });
+
+
+
+  $('.s-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    vertical: true,
+    verticalSwiping:true,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay:false,
+    prevArrow:"<i class='fa fa-chevron-up b-up'></i>",
+    nextArrow:"<i class='fa fa-chevron-down b-down'></i>",
+
+  });
+
+  
